87eb9546ffd34468b26fe465a9318b525c2b8053
João Oliveira <joaoliveira9@outlook.com>
2024-03-16 12:40:51 -0300

    changing the logic and make possible interger operations

--- script.js
+++ script.js
@@ -1,131 +1,102 @@
 /*
 *! selectin elements of the DOM
 */
-//selection DOM element with id number
+//selection DOM element with querySelector(not have to select a specific element) number
 const numberButtons = document.querySelector("#numbers");
-
-//selection DOM element with id symbols
-const symbolsButtons = document.getElementById("symbols");
 
 //selection DOM element with id commands
 const commandButtons = document.getElementById("commands");
 
-let number1, numero1;//:string
-let operationSymbol;//:string
-let number2,numero2;//:string
-let result;
+const resultButton = document.getElementById("value")
 
-console.log(numberButtons);
+let contDot = 0;
+let input = [];
+let command;
+let operand;
+let i = 0;
+let number1;
+let number2;
+let result = 32111;
 
-//using a object event to catch the componers of a container,with this i will use a eventListener for every button, the e object especify what div we talking a about
 numberButtons.addEventListener('click',function(e){
-  //string value for number1
-  number1 = e.target.textContent;
-/*   console.log("passei aqui"); */
-  //verify if the string corresponds a number or a leater
-  if(!isNaN(number1))
-  {
-     numero1 = parseInt(number1);
-      symbolsButtons.addEventListener('click',function(e){
-      operationSymbol = e.target.textContent;
-      if(isNaN(operationSymbol)){
-        numberButtons.addEventListener('click',function(e){
-          number2 = e.target.textContent
-          if(!isNaN(number2))
-          {
-            numero2 = parseInt(number2);
-            switch(operationSymbol)
-            {
-              case("+"): 
-              {
-                result = numero2 + numero1 
-                alert(result);
-                break;
-              }
-              default:
-                alert("deu errado")
-                break
-            }
-          }
-        })
-      }
-    })
-  }
+    //string que vai ter os numeros de entrada
+    input[i] = e.target.textContent;
+    
+    if(document.getElementById("result").textContent == "0"){
+        document.getElementById("result").textContent = input[i];
+    }
+    else
+        document.getElementById("result").textContent = document.getElementById("result").textContent +  input[i] ;
+
+    
+    i++;
+    /* console.log(input); */
+})
+ 
+commandButtons.addEventListener('click',function(e){
+    //seleciona a operação que vai ser realizada
+    command = e.target.textContent;
+    document.getElementById("result").textContent = "0";
+    
+    //transforma o array em uma string que não tem separador e em seguida converte para um numero
+    operand = input.join('');
+    operand = parseInt(operand);
+
+    
+  
+    if(contDot == 0){
+        number1 = operand;
+        contDot++;
+    }
+   
+
+    input = [];
+    input.length = 0;
+        
+  /*   console.log(operand); */
 })
 
+resultButton.addEventListener('click',function(e){
+    operand = input.join('');
+    operand = parseInt(operand);
 
+    if(contDot == 1){
+        number2 = operand;
+        contDot--;
+    }
+     let teste = number1 + number2
 
+     /* console.log(command) */
+    selectinOperation(command);
+    document.getElementById('result').textContent = result;
 
-
-
-///
-
-/* var totalNumber = document.getElementById("total");
-totalNumber.textContent = "0";
-
-
-
-document.getElementById("numbers").addEventListener('click', function(){
-    number += this.textContent;
-    totalNumber.textContent = number;
 })
 
-//
-number  = "";
-
-document.getElementById("numbers").addEventListener('click', function(e){
-    number += e.target.textContent;
-    totalNumber.textContent = number;
-}) */
-
-
-/* <!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Useful event target example</title>
-    <style>
-      .tile {
-        height: 100px;
-        width: 25%;
-        float: left;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="container">
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-     <div class="tile"></div>
-   </div>
-
-    <script>
-      function random(number) {
-        return Math.floor(Math.random()*number);
-      }
-
-      function bgChange() {
-        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-        return rndCol;
-      }
-
-      const container = document.querySelector('#container');
-      container.addEventListener('click', event => event.target.style.backgroundColor = bgChange());
-    </script>
-  </body>
-</html>
- */
+function selectinOperation(operationValue)
+{
+   
+    switch(operationValue)
+    {
+        case '-':
+        {
+            result = number1 - number2;
+            break
+        }
+        case '+':
+        {
+            result = number1 + number2;
+            break
+        }
+        case 'x':
+        {
+            result = number1 * number2;
+            break        
+        }
+        case '+':
+        {
+            result = number1 / number2;
+            break
+        }
+    }
+ 
+}
